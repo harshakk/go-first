06dab3d921b4d5c8ebcb352e56c7d3e30ea861b3
harshakk <harsha.kodlaya@live.com>
2017-04-14 18:16:40 +0530

    Commit to Main 01:

--- hello-go.go
+++ hello-go.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello you sweet little Jackass - Branch!\n")
+	fmt.Printf("Hello you sweet little Jackass!\n")
+	fmt.Printf("Hello you sweet little Asshole!\n")
 }
