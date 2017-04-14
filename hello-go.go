ac52186c3731f7104fd2dc3f03f6866ff5a2fb08
harshakk <harsha.kodlaya@live.com>
2017-04-14 18:29:06 +0530

    Update hello-go.go
    Syncing up Main and Branch

--- hello-go.go
+++ hello-go.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello you sweet little Jackass - Branch!\n")
-	fmt.Printf("Hello you sweet little Jackass - Fucker!\n")
+	fmt.Printf("Hello you sweet little Jackass!\n")
+	fmt.Printf("Hello you sweet little Asshole!\n")
 }
